555a01d62fd374718a9e5eefb82d824bac1d7cdc
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-06-12 00:48:08 -0600

    Create Transformation Pipelines.py

---
+++ Transformation Pipelines.py
@@ -0,0 +1,19 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+num_pipeline = Pipeline([
+('imputer', SimpleImputer(strategy="median")),
+('attribs_adder', CombinedAttributesAdder()),
+('std_scaler', StandardScaler()),
+])
+dataset_num_tr = num_pipeline.fit_transform(dataset_num)
+
+
+# one transformer to take care of all numeric and categorical columns:
+from sklearn.compose import ColumnTransformer
+num_attribs = list(dataset_num) # get numeric columns
+cat_attribs = ["categorical_columns"] # get catgorical columns
+full_pipeline = ColumnTransformer([
+("num", num_pipeline, num_attribs),
+("cat", OneHotEncoder(), cat_attribs),
+])
+dataset_prepared = full_pipeline.fit_transform(dataset)
