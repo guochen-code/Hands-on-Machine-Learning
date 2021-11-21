c5bc5dd010287742191c9098bf341ef95057e9ed
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-21 06:52:33 -0700

    Create Missing_values.py

---
+++ Missing_values.py
@@ -0,0 +1,8 @@
+(1) missing values can be hidden from us and replaced by other value such as -1 / plot histogram to identify any unusal peaks.
+(2) fillna approaches:
+  2.1 -999,-1 etc:
+  2.2 mean, median:
+  2.3 reconstruct value: 
+(3) create isnull feature.
+(4) be very careful when generating new feature using columns with missing values. for example, ignore missing values when calculate means for each category.
+(5) sometimes we can treat outliers as missing values.
