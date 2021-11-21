3e628dbf9a542357a9e3dcfdbe985cc7829f5d5d
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-21 07:14:39 -0700

    Update KC_Missing_values.py

--- KC_Missing_values.py
+++ KC_Missing_values.py
@@ -1,4 +1,4 @@
-(1) missing values can be hidden from us and replaced by other value such as -1 / plot histogram to identify any unusal peaks.
+(1) missing values can be hidden from us and replaced by other value such as -1 or already replaced by the mean value / plot histogram to identify any unusal peaks.
 (2) fillna approaches:
   2.1 -999,-1 etc:
   2.2 mean, median:
@@ -6,3 +6,4 @@
 (3) create isnull feature.
 (4) be very careful when generating new feature using columns with missing values. for example, ignore missing values when calculate means for each category.
 (5) sometimes we can treat outliers as missing values.
+(6) xgb can handle NAN.
