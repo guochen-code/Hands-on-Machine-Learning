11a001a7d959610f5e182af51a5843f19d326183
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-12-08 16:09:57 -0700

    Update Feature_generation_in_batches.py

--- Feature_generation_in_batches.py
+++ Feature_generation_in_batches.py
@@ -16,3 +16,7 @@ pf.fit(df[features])
 pf.get_feature_names() ##################################################################################### return x0,x1,x0^2,X0 X1, X1^2
 feature_array=pf.transform(df[features])
 pd.DataFrame(feature_array,columns=pf.get_feature_names(input_features=features)) ############################ return real names
+
+#### PolynomialFeatures has to be done prior to scaler in pipeline: if you standardize first you will end up with negative and positive values 
+for values that have all been positive, therefore changing all the interaction terms. Also bring them down to a smaller scale. so rather than the first value times the second
+value both being above 1 and therefore increasing the value even more. If you scale it down to values that are maybe 0.5 and 2, we are actually reducing the value.
