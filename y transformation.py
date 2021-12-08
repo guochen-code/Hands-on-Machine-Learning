d5f109dafc2898552286bbea099bc5ef45bad72b
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-12-08 16:07:10 -0700

    Create y transformation.py

---
+++ y transformation.py
@@ -0,0 +1,5 @@
+linear regression is going to assume normally distributed residuals, not y variable, but it can be aided by transforming y variable.
+
+Inverse transformation: because of previous transformation, y prediction and y are not on the same scale. we have to get them back on the same scale as our original numbers.
+  
+
