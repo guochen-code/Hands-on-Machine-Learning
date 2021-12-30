bdd8e779cad16345fd61a79008db6973a254cc9c
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-12-29 23:27:10 -0700

    Create Abstract class and method.py

---
+++ Abstract class and method.py
@@ -0,0 +1,5 @@
+abstract class are classes that contain one or more abstract methods.
+an abstract method is a method that is declared, but contains no implementation.
+abstract classes cannot be instantiated, and require subclasses to probide implementation for the abstract methods.
+subclasses of an abstract class in python are not required to implement abstract methods of the parent class.
+
