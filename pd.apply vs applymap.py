94959a71e3b89929865252e181b830adf3907a1c
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-02-12 20:45:39 -0700

    Update pd.apply vs applymap.py

--- pd.apply vs applymap.py
+++ pd.apply vs applymap.py
@@ -9,3 +9,13 @@ def toInch(val):
     return val
   
   df.applymap(toInch)
+
+##################### apply works on a row/column basis #######################
+def den(rb, rf=1, rm=2.71):
+    return (rm-rb)/(rm-rf)
+dfl['Den_por'] = dfl['RHOB'].apply(den, rf=1, rm=2.71)
+
+
+##################### lambda #####################
+x=lambda a,b: (a+b)/2
+df_dropped['PHIT']=x(df_dropped['NPHI'],df_dropped['Den_por'])
