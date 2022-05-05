5a7246d8765c1cd0c79d92b7ee35e6e59fa0f772
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-05-05 11:25:35 -0600

    Create write to csv.py

---
+++ write to csv.py
@@ -0,0 +1,23 @@
+# prepare data
+import csv 
+data = [1,2,3,4,5] 
+
+
+# define function
+from csv import writer
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+        
+        
+# write to the csv file     
+append_list_as_row('odd_test.csv', data)
+
+# transpose
+import pandas as pd
+df=pd.read_csv('odd_test.csv',header=None)
+df.transpose()
