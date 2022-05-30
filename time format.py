641f5117c6cec32f229be6eb63183080b08f4569
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-05-30 12:23:32 -0600

    Update time format.py

--- time format.py
+++ time format.py
@@ -1,4 +1,17 @@
 datetime.today().strftime("%Y-%m-%d %H:%M:%S %p %a")
-
 # output 
-# '2022-05-30 12:11:24 PM Mon'
+# '2022-05-30 12:20:39 PM Mon'
+
+
+datetime.today().strftime("%Y-%m-%d %H:%M:%f %p %a")
+# output 
+# '2022-05-30 12:20:397326 PM Mon'  ############################### wrong format
+
+
+datetime.today().strftime("%Y-%m-%d %H:%M:%S.%f")
+# output 
+# '2022-05-30 12:22:15.804398'
+
+datetime.today().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+# output 
+# '2022-05-30 12:23:04.524'
