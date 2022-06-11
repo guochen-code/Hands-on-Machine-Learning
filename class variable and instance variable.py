87156ab9e318cdae0721a0e5ad5b526e153f0db6
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-06-10 23:39:11 -0600

    Update class variable and instance variable.py

--- class variable and instance variable.py
+++ class variable and instance variable.py
@@ -19,3 +19,31 @@ water = Water(temperature=20)
 water.boiling_temperature = 212
 water.freezing_temperature = 32
 print(water.state())
+
+
+******************************************************************************************************************************************************
+
+class Matter:
+    boiling_temperature = None
+    freezing_temperature = None
+
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+    def state(self):
+        if self.temperature <= self.freezing_temperature:
+            return 'solid'
+        elif self.freezing_temperature < self.temperature < self.boiling_temperature:
+            return 'liquid'
+        else:
+            return 'gas'
+
+
+class Water(Matter):
+    boiling_temperature = 100
+    freezing_temperature = 0
+
+
+class Mercury(Matter):
+    boiling_temperature = 356.7
+    freezing_temperature = -38.83
