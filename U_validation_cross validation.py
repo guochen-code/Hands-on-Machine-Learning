b9524bfd8f01eae0022741c601807e34b3d43c44
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-06-29 12:17:20 -0600

    Create U_validation_cross validation.py

---
+++ U_validation_cross validation.py
@@ -0,0 +1,11 @@
+(1) what is the problem before validation set?
+evaluate each model performance according to test data set. Data leakage. Overfitting.
+
+(2) what is the new problem after validation set?
+1. little data to train the model
+2. each model has only one performance, no min or max performance for each model
+
+(3) what is the problem before cross validation set? == (2)
+
+(4) what is the new problem after cross validation set?
+
