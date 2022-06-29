9c91bc130a09c86010cbc953a57c9c5a40cd2a2a
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-06-29 14:44:13 -0600

    Create U_hyperparameters.py

---
+++ U_hyperparameters.py
@@ -0,0 +1,5 @@
+when we create hyperparameters sampling strategies we need to consider:
+  - number of hyperparamters of the machine learning model
+  - the low effective dimension
+  - the nature of the parameters (discrete, continuous, categorical)
+  - the computing resources available to us
