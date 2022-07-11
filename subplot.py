bf97c5316ffdd709211773d545e2efc2f0df17e5
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-07-11 13:40:51 -0600

    Update subplot.py

--- subplot.py
+++ subplot.py
@@ -4,3 +4,19 @@ df[field].apply(np.log1p).hist(ax=ax_after)
 ax_before.set(title='title',ylabel='ylabel',xlabel='xlabel')
 ax_after.set(title='title',ylabel='ylabel',xlabel='xlabel')
 fig.suptitle('suptitle')
+
+# fig - ax - all plots on one figure
+def automate_plot(df, variable, target):
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    for category in df[variable].unique():
+        df[df[variable]==category][target].plot(kind='kde', ax=ax)
+    
+    # add the legend
+    lines, labels = ax.get_legend_handles_labels()
+    labels = df[variable].unique()
+    ax.legend(lines, labels, loc='best')
+    
+    plt.show()
