56f6b7104b36c1de0417f1fb1bd3ec3d597338d9
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-08-25 16:55:40 -0600

    Update pycharm terminal.py

--- pycharm terminal.py
+++ pycharm terminal.py
@@ -3,6 +3,10 @@ PS C:\Users\CGuo\PycharmProjects\pythonProject2-PyQT> python -m pyinstaller
 C:\Users\CGuo\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Pytho
 n.3.9_qbz5n2kfra8p0\python.exe: No module named pyinstaller
 
+# in conclusion, found:
+pycharm terminal is pointed to C:\Users\CGuo\AppData\Local\Microsoft\WindowsApps
+you have pyinstaller.exe in this folder, then it will run!  
   
+#  
 jupyterlab
 C:\Users\CGuo\Anaconda3\Scripts\pyinstaller.exe         -> able to run
