e768db63485bb89439d852067ee451ec45d2432c
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-08-25 16:41:04 -0600

    Create pycharm terminal.py

---
+++ pycharm terminal.py
@@ -0,0 +1,8 @@
+# not able to run
+PS C:\Users\CGuo\PycharmProjects\pythonProject2-PyQT> python -m pyinstaller     
+C:\Users\CGuo\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Pytho
+n.3.9_qbz5n2kfra8p0\python.exe: No module named pyinstaller
+
+  
+jupyterlab
+C:\Users\CGuo\Anaconda3\Scripts\pyinstaller.exe         -> able to run
