c7972920a4d4d8068ae1161db6676cef955e3ddf
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-08-25 19:05:21 -0600

    Update Multiprocessing-queue.py

--- Multiprocessing-queue.py
+++ Multiprocessing-queue.py
@@ -44,3 +44,51 @@ if __name__=='__main__':
         for i in q_lst.keys():
             if message['id'] == i:
                 q_lst[i].put(message)
+
+
+**************************************************** auto scale ****************************************************
+from multiprocessing import Process, Queue
+import time
+import sys
+import random
+
+
+def reader_proc(queue,name):
+    ## Read from the queue; this will be spawned as a separate Process
+    while True:
+        msg = queue.get()         # Read from the queue and do nothing
+        print(f'name {name}:',msg)
+        if (msg == 'DONE'):
+            break
+
+if __name__=='__main__':
+    name_lst=['111','222','333','444','555','666']
+    all_active_process={}
+    all_active_queue={}
+    while True:
+        time.sleep(1)
+        message = {'id': random.choice(name_lst), 'value': random.random()}
+        print('generated message:',message)
+
+        if message['id'] not in all_active_process.keys():
+            all_active_queue[message['id']]=Queue()
+            all_active_process[message['id']] = Process(target=reader_proc, args=((all_active_queue[message['id']]), message['id']))
+            all_active_process[message['id']].start()
+
+        for i in all_active_queue.keys():
+            if message['id'] == i:
+                all_active_queue[i].put(message)
+'''
+generated message: {'id': '222', 'value': 0.8496754167881175}
+name 222: {'id': '222', 'value': 0.8496754167881175}
+generated message: {'id': '555', 'value': 0.6130036145157562}
+name 555: {'id': '555', 'value': 0.6130036145157562}
+generated message: {'id': '444', 'value': 0.7538112166666542}
+name 444: {'id': '444', 'value': 0.7538112166666542}
+generated message: {'id': '444', 'value': 0.719968479151165}
+name 444: {'id': '444', 'value': 0.719968479151165}
+generated message: {'id': '222', 'value': 0.9858231576376857}
+name 222: {'id': '222', 'value': 0.9858231576376857}
+generated message: {'id': '111', 'value': 0.05165566336301364}
+name 111: {'id': '111', 'value': 0.05165566336301364}
+'''
