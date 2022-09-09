4973e6dc12781f7b51ad3b7ad09bd1c339f7ddbf
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-09-09 10:01:20 -0600

    Update timezone.py

--- timezone.py
+++ timezone.py
@@ -3,5 +3,5 @@ from datetime import datetime, tzinfo, timedelta
 
 my_var = -5 # GMT-5
 UTC_timezone=pytz.timezone('UTC')
-rig_time = datetime.now(rig_timezone) + timedelta(hours= my_var)
+rig_time = datetime.now(UTC_timezone) + timedelta(hours= my_var)
 rig_time.strftime('%Y-%m-%d %I:%M:%S %p')
