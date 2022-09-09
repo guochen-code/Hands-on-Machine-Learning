3550e2a80d499f3f51c2c73fcf16d522cd23b809
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-09-09 10:05:45 -0600

    Update timezone.py

--- timezone.py
+++ timezone.py
@@ -5,3 +5,9 @@ my_var = -5 # GMT-5
 UTC_timezone=pytz.timezone('UTC')
 rig_time = datetime.now(UTC_timezone) + timedelta(hours= my_var)
 rig_time.strftime('%Y-%m-%d %I:%M:%S %p')
+
+
+# from timestamp in miliseconds, e.g. 1662739495000
+utc_dt = datetime.utcfromtimestamp(ts / 1000.0)
+rig_time = utc_dt + timedelta(hours=gmt_offset)
+connection_time = rig_time.strftime('%Y-%m-%d %I:%M:%S %p')
