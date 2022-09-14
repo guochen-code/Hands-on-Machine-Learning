f66d760647b69e440482890d4af48e432862a0ec
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-09-13 19:42:19 -0600

    Create Multiprocessing-number of workers.py

---
+++ Multiprocessing-number of workers.py
@@ -0,0 +1,14 @@
+length=100
+number_of_process=4
+
+for i in range(number_of_process):
+    lower=i*length/4
+    upper=(i+1)*length/4
+    print(lower,upper)
+
+'''
+0.0 25.0
+25.0 50.0
+50.0 75.0
+75.0 100.0
+'''
