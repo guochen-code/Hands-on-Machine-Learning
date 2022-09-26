da813a2a673ddd068a84196451ab2f564500cd1c
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-09-26 17:13:18 -0600

    Update Read data from database.py

--- Read data from database.py
+++ Read data from database.py
@@ -43,3 +43,28 @@ full_df = pd.concat(dfs)
 
 #It might also be possible that the whole dataframe is simply too large to fit in memory, 
 # in that case you will have no other option than to restrict the number of rows or columns you're selecting.
+********************************************************************************************************************************
+# Fastest way to read Cassandra data into pandas with automatic iteration of pages. 
+# Create dictionary and add each to it by automatically iterating all pages. Then, create dataframe with this dictionary.
+
+import pandas as pd
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.query import dict_factory
+
+auth_provider = PlainTextAuthProvider(username=CASSANDRA_USER, password=CASSANDRA_PASS)
+cluster = Cluster(contact_points=[CASSANDRA_HOST], port=CASSANDRA_PORT,
+    auth_provider=auth_provider)
+
+session = cluster.connect(CASSANDRA_DB)
+session.row_factory = dict_factory
+
+sql_query = "SELECT * FROM {}.{};".format(CASSANDRA_DB, CASSANDRA_TABLE)
+
+dictionary ={"column1":[],"column2":[]}
+
+for row in session.execute(sql_query):
+    dictionary["column1"].append(row.column1)
+    dictionary["column1"].append(row.column1)
+
+df = pd.DataFrame(dictionary)
