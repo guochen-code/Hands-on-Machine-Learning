c3b3a47731657a5bf5c3e7f569aefcaf0568a8a2
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-11-03 15:09:16 -0600

    Create U-git.py

---
+++ U-git.py
@@ -0,0 +1,105 @@
+**************************************************************************** initialization
+# check current configuration
+git config user.name
+git config user.email
+
+# create new configuration
+git config --global user.name "user"
+git config --global user.email "email"
+
+# before try git init, check:
+git status # make sure you not create a new init(repo) in an existing init(repo), no warming, but enventually will break
+
+#initialization, use once per project
+git init
+
+# github - public vs private
+# public
+git clone https://github.com/guochen-code/my_second_repo.git
+# private
+git clone https://github.com/guochen-code/my_private_repo.git
+#above command will not work, will show could not find the repo
+
+# clone syntax with PAT: # go to developer setting in github and generate token
+git clone https://<token@>github.com/guochen-code/my_private_repo.git
+
+**************************************************************************** lcaol -- git add and git commit
+git add file_one.txt file_two.txt
+git commit -m "add file_one and file_two"
+git add . # all all files in the directory
+# press q button to quit the log view
+**************************************************************************** github -- git remote and git push
+git remote add origin https://github.com/guochen-code/my_second_repo.git # origin is default name by convention
+# not everyone is allowed to upload files to github, need a token. so above origin will not work. correct command is:
+git remote add origin https://<token@>github.com/guochen-code/my_second_repo.git
+  
+git remote remove origin # remove the origin
+git remote -v # check what is the remote origin
+git push -u origin master # push from local to github
+
+**************************************************************************** git fetch and git pull (==git fetch and git merge)
+# git fetch makes sense when you are working with others and not ready to override your own files in your local computer
+# when you are travelling, you added a new file on github.
+# when you back home, you want to see this files from your local machine through your IDE like pycharm instead ot web browser
+git checkout origin/master # must have this origin in place # need to run git fetch prior to this command!!!!!!!!
+# so correct steps will be:
+git fetch
+git checkout origin/master
+# after above, you now are able to see the new file created on github. BUT NOTE up to now it didnot override your local working directory, you can just see it
+# to undo, switch back to where we were
+git switch master. # failed in my case, we can discard this new file by running: git checkout github_file.txt and then switch back to master and then can pull
+
+# alternatively
+git pull
+
+**************************************************************************** git branch: create/switch/delete
+git branch <my_branch_name>
+git switch <my_branch_new>
+
+git branch -d <my_branch_name> # use this one, because it will alert you if this branch is not merged before deleting it
+git branch -D <my_branch_name> # will be forced to delete
+
+**************************************************************************** merge
+# fast forward merge: why fast forward? master have no commits, all the commits happened in the new_branch
+# multiple branch commit merge with no conflict: if starting point the same, then create a new file in new branch, create a new file in master, merge, no conflict
+# multiple branch commit merger with a conflict
+
+# first to be on master branch
+git merge <my_branch_name> # if with conflict, need to solve before auto merge can be executed. use PROPER editor to solve the conflict!!!!!!!!
+
+**************************************************************************** git diff
+# check your current files vs your latest commiited files
+# when you make some changes but have not committed, you go away and come back from a coffee break, you don't remember what you did. 
+# use: (this is in the same branch)
+git diff
+
+# how to use git diff for different branches?
+git diff <branch_1> <branch_2>
+
+
+**************************************************************************** git checkout: just go back and view the old files, not change anything
+# easier way to find hash instead of use git log
+git log --oneline
+
+git checkout <hash_number>
+
+# how to undo
+git checkout master # go back to master and latest commit
+
+**************************************************************************** git restore
+# git restore filename
+# git restore --source HEAD~N filename # if you at 3rd commit, N=2 means you go back to the 1st commit
+# git restore --staged filename # after execute git add filename, you want to revoke this action
+
+**************************************************************************** git rest
+# you just reset the commit, the news files still there!!!!!!!!!!
+# this is useful if you accidentally committed to the wrong branch (for example, maybe you forgot to
+# run git switch right after creating a new branch, accidentally committing to the original branch).
+git reset <hash>
+
+# what if you want the files to change:
+git reset <hash> --hard
+
+****************************************************************************
+
+
