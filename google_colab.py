a6ef39bc439df433df65a99acb3aa4830f9f6cfd
guochen-code <83231868+guochen-code@users.noreply.github.com>
2025-01-09 14:24:20 -0700

    Update google_colab.py

--- google_colab.py
+++ google_colab.py
@@ -6,6 +6,43 @@
 !kill -9 $(lsof -t -i:6008)
 
 
+###################################################### code correction
+from tensorboard.plugins import projector
+
+projector_config = projector.ProjectorConfig()
+# projector_config = tf.compat.v1.summary.ProjectorConfig()
+embedding_config = projector_config.embeddings.add()
+embedding_config.tensor_name = "embedding/.ATTRIBUTES/VARIABLE_VALUE"
+embedding_config.metadata_path = metadata_file
+tf.compat.v1.disable_eager_execution()  # to use old summary writer
+summary_writer = tf.compat.v1.summary.FileWriter(embedding_log_dir)
+projector.visualize_embeddings(summary_writer, projector_config)
+print("Run `tensorboard --logdir=./logs` to visualize the embedding in TensorBoard projector.")
+summary_writer.close()
+###################################################### 
+
+###################################################### not working TF==2.11
+# tf.keras.utils.Progbar
+# # Set up config for the projector
+# config = tf.compat.v1.keras.utils.progbar_utils.ProgbarConfig(
+#     embeddings=[
+#         tf.compat.v1.keras.utils.ProgbarConfig.Embedding(
+#             tensor_name='embedding/.ATTRIBUTES/VARIABLE_VALUE',
+#             metadata_path='metadata.tsv',
+#         )
+#     ]
+# )
+
+# with open(os.path.join(logdir, 'projector_config.pbtxt'), 'w') as f:
+#     f.write(str(config))
+######################################################
+
+
+
+
+###################################################### ###################################################### ###################################################### ------------------
+
+
 
 # downgrade python version on colab
 !sudo apt-get install python3.7
